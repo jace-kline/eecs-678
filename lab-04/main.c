3f1a11e8a4c8cd77c9586ae738d4197ef94ddbdf
jace-kline <jace_kline@ku.edu>
2020-02-20 00:13:51 -0600

    lab 4 iter 1, not working

--- lab-04/main.c
+++ lab-04/main.c
@@ -1,5 +1,7 @@
 // This code is implemented for fourth lab of EECS 678 Lab - University of Kansas
 
+#define LAB_CODE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -12,6 +14,7 @@
 // Recursive file counter
 int getNumOfFilesRec(char *path);
 
+// We assume argv[1] is the path to the directory to count
 int main(int argc,char* argv[]) {
     int numOfFiles=0;
 
@@ -24,6 +27,26 @@ int main(int argc,char* argv[]) {
     // Create a pipe,fork, and call the function. The child will write the returned
     // value(number of files) to the parent. Parent should read it and wait for child
     // to finish.
+    int pipe_fd[2]; // pipe_fd[0] ~ read , pipe_fd[1] ~ write
+    pipe(pipe_fd);
+    pid_t id = fork();
+     switch (id)
+     {
+        case -1: {// Error
+            perror("Failed to create fork in function 'main'.");
+            break;
+        }
+        case 0: {// Child Process
+            int ret = getNumOfFilesRec(argv[1]);
+            write(pipe_fd[1], &ret, sizeof(int));
+            exit(0);
+        }
+        default: {// Parent Process ( id > 0 )
+            read(pipe_fd[0], &numOfFiles, sizeof(int));
+            wait(NULL);
+            break;
+        }
+     }
 
 #else
     numOfFiles = getNumOfFilesRec(argv[1]);
@@ -47,6 +70,11 @@ int getNumOfFilesRec(char path[PATH_MAX]){
 #ifdef LAB_CODE
     // Create a pipe for all children. No need to create an external
     // pipe for each children.
+    int fd[2];
+    pipe(fd);
+    int num_children = 0;
+    pid_t pid;
+    int ret;
 
 #endif
 
@@ -77,7 +105,24 @@ int getNumOfFilesRec(char path[PATH_MAX]){
             // Create a child process to get into the directory
             // to count files. Write the returned number of files
             // to parent via pipe
+            pid = fork();
 
+            switch (pid)
+            {
+                case -1: {// ERROR
+                    perror("Failed to create fork in function 'getNumOfFilesRec'.");
+                    break;
+                }
+                case 0: {// Child process
+                    getNumOfFilesRec(targetPath);
+                    read(fd[0], &ret, sizeof(int));
+                    break;
+                }
+                default: {// Parent process
+                    ++num_children;
+                    break;
+                }
+            }
 #else
             int returnVal = getNumOfFilesRec(targetPath);
             if (returnVal == -1) {
@@ -98,6 +143,13 @@ int getNumOfFilesRec(char path[PATH_MAX]){
 #ifdef LAB_CODE
     // read the number of files integer coming from children and
     // wait all of them to finish execution
+    //write(fd[1], )
+    for(int i = 0; i < num_children; i++) {
+        int tmp;
+        read(fd[0], &tmp, sizeof(int));
+        wait(NULL);
+        numOfFiles += tmp;
+    }
 
 #endif
 
